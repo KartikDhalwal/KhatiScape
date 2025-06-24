207b1888ecd9404c8d9dea0cf879b8ae92bf5d03
KartikDhalwal <kartikdhalwal145@gmail.com>
2025-06-25 00:24:10 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   eslint: {
+    ignoreDuringBuilds: true, // Disable linting during builds
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Disables TypeScript errors during build
+  },
+   webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+  images: {
+    domains: ['images.unsplash.com'], // Add your image domains here
+  },
 };
 
 export default nextConfig;
